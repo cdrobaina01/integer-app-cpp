0cf789f07b38fa7506e87d10a4a56970be732311
MAPS <peraza011024@gmail.com>
2023-05-07 20:21:55 -0400

    Probando operador +

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include "MainWindow.h"
 #include <QApplication>
+#include <Integer.h>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    /*QApplication a(argc, argv);
     MainWindow w;
     w.show();
 
-    return a.exec();
+    return a.exec();*/
+
+
+    //Prueba
+    int array1[4] = {4,3,9}; // 934
+    int array2[2] = {7,6};   //  67
+                             //1001
+    Integer num1(array1, sizeof(array1)/sizeof(int));
+    Integer num2(array2, sizeof(array2)/sizeof(int));
+
+    Integer num3(num1+num2);
+
+    cout << "La suma de los dos numeros es:" << num3.Get() << endl;
+
+    return 0;
 }
